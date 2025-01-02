b48552440952df87ffda7d32ce4795f4ba29bf5f
saurabhgdeshmukh <saurabhgdeshmukh@gmail.com>
2025-01-02 23:35:57 +0530

    Signup Added

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Auth } from "../components/Auth"
+import { Quote } from "../components/Quote"
 
-function Signup() {
-  return (
-    <div>Signup</div>
-  )
+export const Signup = () => {
+    return <div>
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+            <div>
+                <Auth type="signup" />
+            </div>
+            <div className="hidden lg:block">
+                <Quote />
+            </div>
+        </div>
+    </div>
 }
-
-export default Signup
