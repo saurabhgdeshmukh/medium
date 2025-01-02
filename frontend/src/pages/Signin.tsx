f6fdf7029d3ef386efc4e35ab42f4843158c547b
saurabhgdeshmukh <saurabhgdeshmukh@gmail.com>
2025-01-02 23:37:00 +0530

    Signin Added

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Auth } from "../components/Auth"
+import { Quote } from "../components/Quote"
 
-function Signin() {
-  return (
-    <div>Signin</div>
-  )
+export const Signin = () => {
+    return <div>
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+            <div>
+                <Auth type="signin" />
+            </div>
+            <div className="hidden lg:block">
+                <Quote />
+            </div>
+        </div>
+    </div>
 }
-
-export default Signin
